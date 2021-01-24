0a9de26978ff6e7c74dae887c1f753ccb3c19e1e
qudanil <50636224+qudanil@users.noreply.github.com>
2021-01-24 14:05:50 +0700

    Update afk.py

--- haruka/modules/afk.py
+++ haruka/modules/afk.py
@@ -87,7 +87,7 @@ def check_afk(bot, update, user_id, fst_name):
         if not user.reason:
             res = tld(chat.id, f"{fst_name} is AFK!")
         else:
-            res = tld(chat.id, f"{fst_name} is AFK! says its because of:\n{user.reason}")
+            res = tld(chat.id, f"{fst_name} Sedang AFK karena :\n{user.reason}")
         update.effective_message.reply_text(res)
 
 
